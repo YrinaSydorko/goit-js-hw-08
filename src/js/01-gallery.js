78acc4b668884d1ba3859f262d88018fbece984e
YrinaSydorko <rozaidoma@gmail.com>
2023-05-10 17:25:58 +0300

    add task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
+//import { galleryItems } from './gallery-items';
+// Change code below this line
+const galleryList = document.querySelector(".gallery");
+const createGalleryItem = ({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}" data-original-img=${original}>
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>
+`;
+
+galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+});
+galleryList.addEventListener("click", (event) => {
+  event.preventDefault();
+  lightbox.open();
+});
 console.log(galleryItems);
+
+
+
+
+
